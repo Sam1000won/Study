54dd024148133bf5e41d05b419c76e717b6d129d
jiughan <135206238+Sam1000won@users.noreply.github.com>
2024-10-21 15:02:11 +0900

    Add files via upload

---
+++ java/java_01/src/Test01/Main.java
@@ -0,0 +1,20 @@
+package Test01;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = 0;
+        Solution sol = new Solution();
+
+        result = sol.solution(70);
+        System.out.println(result);
+        
+        result = sol.solution(91);
+        System.out.println(result);
+
+        result = sol.solution(90);
+        System.out.println(result);
+
+        result = sol.solution(100);
+        System.out.println(result);
+    }
+}
