795ecc979497fe9c9a2d0cbf289c8a8183d68b11
jiughan <135206238+Sam1000won@users.noreply.github.com>
2024-10-22 15:05:02 +0900

    오늘의 자바 공부
    클래스 객체화 연습

---
+++ Java/Java_Study_03/Ex06/Sub.java
@@ -0,0 +1,16 @@
+package Java_Study_03.Ex06;
+
+public class Sub {
+    static int sField = 134;
+    int field = 1;
+
+    static int plus(int a, int b){
+        return a+b;
+    }
+    static int minus(int a, int b){
+        return a-b;
+    }
+    int multi(int a, int b){
+        return a*b;
+    }
+}
